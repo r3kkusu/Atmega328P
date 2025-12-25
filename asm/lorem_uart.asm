9b03e63ec6ec23c5f1d8eb8cc170cd80a3a6e539
r3kkusu <rexjamesp@gmail.com>
2025-12-25 22:07:32 +0800

    Lorem ipsun asm code

---
+++ asm/lorem_uart.asm
@@ -0,0 +1,137 @@
+; =========================================
+; File: lorem_uart.asm
+; ATmega328P
+; PURE ASM: FLASH → SRAM → UART
+; No .S, no pm(), no CPP
+; =========================================
+
+; -------------------------
+; Register definitions
+; -------------------------
+.equ UBRR0H,  0xC5
+.equ UBRR0L,  0xC4
+.equ UCSR0A,  0xC0
+.equ UCSR0B,  0xC1
+.equ UCSR0C,  0xC2
+.equ UDR0,    0xC6
+
+.equ TXEN0,   3
+.equ UDRE0,   5
+.equ UCSZ00,  1
+.equ UCSZ01,  2
+
+.equ SPL,     0x3D
+.equ SPH,     0x3E
+.equ RAMEND,  0x08FF
+
+.equ UBRRVAL, 103        ; 9600 baud @ 16 MHz
+
+; -------------------------
+; Reset vector
+; -------------------------
+.org 0x0000
+rjmp main
+
+; -------------------------
+; UART Init
+; -------------------------
+uart_init:
+    ldi r16, lo8(UBRRVAL)
+    sts UBRR0L, r16
+    ldi r16, hi8(UBRRVAL)
+    sts UBRR0H, r16
+
+    ldi r16, (1 << TXEN0)
+    sts UCSR0B, r16
+
+    ldi r16, (1 << UCSZ01) | (1 << UCSZ00)
+    sts UCSR0C, r16
+    ret
+
+; -------------------------
+; UART Send Char
+; r16 = character
+; -------------------------
+uart_putchar:
+wait_udre:
+    lds r17, UCSR0A
+    sbrs r17, UDRE0
+    rjmp wait_udre
+    sts UDR0, r16
+    ret
+
+; -------------------------
+; UART Print String (SRAM)
+; X points to string
+; -------------------------
+uart_print:
+    ld r16, X+
+    tst r16
+    breq uart_done
+    rcall uart_putchar
+    rjmp uart_print
+uart_done:
+    ret
+
+; -------------------------
+; Copy FLASH → SRAM
+; Z = FLASH BYTE address
+; X = SRAM destination
+; -------------------------
+copy_flash_to_sram:
+copy_loop:
+    lpm r16, Z+
+    st  X+, r16
+    tst r16
+    brne copy_loop
+    ret
+
+; -------------------------
+; Main
+; -------------------------
+.global main
+main:
+    ; Stack init
+    ldi r16, lo8(RAMEND)
+    out SPL, r16
+    ldi r16, hi8(RAMEND)
+    out SPH, r16
+
+    rcall uart_init
+
+    ; ---- LOAD FLASH ADDRESS (WORD → BYTE) ----
+    ldi ZL, lo8(lorem)
+    ldi ZH, hi8(lorem)
+    lsl ZL
+    rol ZH
+
+    ; SRAM destination
+    ldi XL, lo8(lorem_buf)
+    ldi XH, hi8(lorem_buf)
+
+    rcall copy_flash_to_sram
+
+loop:
+    ldi XL, lo8(lorem_buf)
+    ldi XH, hi8(lorem_buf)
+    rcall uart_print
+    rjmp loop
+
+; -------------------------
+; FLASH string (single NUL)
+; -------------------------
+.p2align 1
+lorem:
+    .asciz "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\r\n\
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\r\n\
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.\r\n\
+Duis aute irure dolor in reprehenderit in voluptate velit esse.\r\n\
+Excepteur sint occaecat cupidatat non proident.\r\n\
+Sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n"
+
+; -------------------------
+; SRAM buffer
+; -------------------------
+.section .bss
+lorem_buf:
+    .skip 512
